cfb6602b0b9701ef1a7e23eaa05be2831b24b21d
lmaselli <lisandro_950@hotmail.com>
2018-07-30 14:38:10 -0300

    fix element

--- server/server.erl
+++ server/server.erl
@@ -6,7 +6,8 @@
 pstat() ->
 	receive after 1000 ->
 		Carga = statistics(total_active_tasks),
-		lists:foreach(fun(X) -> {pb,X} ! {node(),Carga} end,nodes())
+		lists:foreach(fun(X) -> {pb,X} ! {node(),Carga} end,nodes()),
+        pb ! {node(),Carga}
 	end,
 	pstat().
 	
@@ -17,7 +18,7 @@ pbalance(Dict) ->
 	receive
 	{Pid,nodo} 	->	Lista = dict:to_list(Dict),
 					Lista = lists:sort(fun({A,B},{C,D}) -> B =< D end,Lista),
-					Pid ! element(2,lists:nth(1,Lista));
+					Pid ! element(1,lists:nth(1,Lista));
 	{Nodo,Carga} -> pbalance(dict:store(Nodo,Carga,Dict))
 	end,
 	pbalance(Dict).
@@ -33,21 +34,11 @@ psocket(Sock,Pid_B) ->
 	end,
 psocket(Sock,Pid_B).
 
-loop() ->
-	receive
-		{tcp,Rte,Msg} ->
-			case string:tokens([X || <<X>> <= Msg]," ") of
-				[A,B,C,D] 	-> gen_tcp:send(Rte,Msg), loop();
-				[A,B,C]		-> ok;
-				[A,B,C]		-> ok;
-				_ -> gen_tcp:send(Rte,"asd"), io:format("[~p]",[Msg])
-			end;
-		X -> io:format("??[~p]~n",[X])
-	end.
-
 iniciador(Nodos) ->
 	lists:foreach(fun(X) -> net_adm:ping(X) end,Nodos).
-init(Port,Nodo) ->
+
+
+init(Port,Nodos) ->
 	{ok,LSock} = gen_tcp:listen(Port,[binary,{active,true}]),
 	Pid_B = spawn(?MODULE,pbalance,[dict:new()]),
 	Pid_D = spawn(?MODULE,dispatcher,[LSock,Pid_B]),
