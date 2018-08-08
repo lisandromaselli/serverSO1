b1e5d4b9759bd975496d9a0396702a3db832444d
sebamoralesasd <sebasuni96@gmail.com>
2018-08-08 19:57:25 -0300

    More changes

--- server/tateti.erl
+++ server/tateti.erl
@@ -3,16 +3,17 @@
 
 loop1() ->
 	receive
-		{p_one, Pid_J1, Jugada, Lista} when Jugada >= 1; Jugada =< 9 -> 
-		spawn(?MODULE,meter,[p_one, Pid_J1, Jugada, Lista]);
-		{p_one, Pid_J1, Jugada, Lista} -> loop1();
-		{status, St, Pl, Lista} ->
-			case St of
-				0 -> io:format("Lista nueva: ~p~n", [Lista]), ok;
-				1 -> ok;
-				-1 -> fin
+		{one, Pid_J1, Jugada, Lista} when Jugada >= 1; Jugada =< 9 -> 
+		Pid_mov = spawn(?MODULE,meter,[one, self(), Jugada, Lista]),
+		receive
+			{status, St, Pl, ListaN} ->
+				case St of
+					0 -> Pid_J1 ! {ListaN};
+					1 -> Pid_J1 ! {one, ListaN};
+					-1 -> Pid_J1 ! {full, ListaN}
+				end;
+		{one, Pid_J1, Jugada, Lista} -> loop1()
 			end;
-			%Pid_J1 ! {"gano",Lista};
 		_ -> loop1()
 	end.
 
@@ -20,7 +21,13 @@ init() ->
 	Lista = [0,0,0,0,0,0,0,0,0],
 	Pid_J = spawn(?MODULE, movem, []),
 	% Pruebas
-	Pid_J ! {p_one, Pid_J, 6, Lista}.
+	Pid_J ! {one, self(), 6, Lista},
+	receive {X} -> io:format("Lista nueva: ~p~n", [X]) end,
+	Pid_J ! {one, self(), 5, X},
+	receive {X1} -> io:format("Lista nueva 2: ~p~n", [X1]) end,
+	Pid_J ! {one, self(), 4, X1},
+	receive {one, X2} -> io:format("Lista nueva 3: ~p~n", [X2]) end,
+	ok.
 
 
 
@@ -35,16 +42,16 @@ meter(Pl, Pid_Pl, Jugada, Lista) ->
 		true ->
 			ListaN = lists:sublist(Lista, Jugada-1) ++ [Pl] ++ lists:nthtail(Jugada, Lista),
 			% Comprobar si alguien gano.
-			io:format("Lista: ~p~n", [ListaN]),
+			%io:format("Lista: ~p~n", [ListaN]),
 			case ListaN of
-				[Pl,Pl,Pl,_,_,_,_,_,_] -> {status, 1, Pl, ListaN};
-				[_,_,_,Pl,Pl,Pl,_,_,_] -> {status, 1, Pl, ListaN};
-				[_,_,_,_,_,_,Pl,Pl,Pl] -> {status, 1, Pl, ListaN};
-				[Pl,_,_,Pl,_,_,Pl,_,_] -> {status, 1, Pl, ListaN};
-				[_,Pl,_,_,Pl,_,_,Pl,_] -> {status, 1, Pl, ListaN};
-				[_,_,Pl,_,_,Pl,_,_,Pl] -> {status, 1, Pl, ListaN};
-				[Pl,_,_,_,Pl,_,_,_,Pl] -> {status, 1, Pl, ListaN};
-				[_,_,Pl,_,Pl,_,Pl,_,_] -> {status, 1, Pl, ListaN};
+				[Pl,Pl,Pl,_,_,_,_,_,_] -> Pid_Pl ! {status, 1, Pl, ListaN};
+				[_,_,_,Pl,Pl,Pl,_,_,_] -> Pid_Pl ! {status, 1, Pl, ListaN};
+				[_,_,_,_,_,_,Pl,Pl,Pl] -> Pid_Pl ! {status, 1, Pl, ListaN};
+				[Pl,_,_,Pl,_,_,Pl,_,_] -> Pid_Pl ! {status, 1, Pl, ListaN};
+				[_,Pl,_,_,Pl,_,_,Pl,_] -> Pid_Pl ! {status, 1, Pl, ListaN};
+				[_,_,Pl,_,_,Pl,_,_,Pl] -> Pid_Pl ! {status, 1, Pl, ListaN};
+				[Pl,_,_,_,Pl,_,_,_,Pl] -> Pid_Pl ! {status, 1, Pl, ListaN};
+				[_,_,Pl,_,Pl,_,Pl,_,_] -> Pid_Pl ! {status, 1, Pl, ListaN};
 				_ -> spawn(?MODULE, isfull, [Pl, Pid_Pl, ListaN]) 
 			end;
 		false -> io:format("Lugar ocupado. Elegir otro.")	%% Repetir jugada en otro lugar
